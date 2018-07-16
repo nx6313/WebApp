9df9cfd01528161d351c1620867190a3b405da46
ngdong <2559635030@qq.com>
2018-07-17 01:22:50 +0800

    dfg

--- app/src/main/java/com/mtxyao/nxx/webapp/MainActivity.kt
+++ app/src/main/java/com/mtxyao/nxx/webapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.mtxyao.nxx.webapp
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.view.KeyEvent
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.mtxyao.nxx.webapp.fragments.ConsoleFragment
 import com.mtxyao.nxx.webapp.fragments.BoardFragment
@@ -24,6 +28,7 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         hideBottomUIMenu()
+        toggleCurMenuIcon()
 
         fragmentPair = listOf(
                 Pair("menu-console", ConsoleFragment()),
@@ -48,6 +53,32 @@ class MainActivity : FragmentActivity() {
             "menu-me" -> curFragmentPageIndex = 4
         }
         viewPager.currentItem = curFragmentPageIndex
+        toggleCurMenuIcon()
+    }
+
+    private fun toggleCurMenuIcon () {
+        for (menuIndex in 0..(footerMenu.childCount - 1)) {
+            val menuImg : ImageView = (footerMenu.getChildAt(menuIndex) as ViewGroup).getChildAt(0) as ImageView
+            val menuTxt : TextView = (footerMenu.getChildAt(menuIndex) as ViewGroup).getChildAt(1) as TextView
+            when (menuIndex) {
+                0 -> menuImg.setImageResource(R.drawable.menu_1_normal)
+                1 -> menuImg.setImageResource(R.drawable.menu_2_normal)
+                2 -> menuImg.setImageResource(R.drawable.menu_3_normal)
+                3 -> menuImg.setImageResource(R.drawable.menu_4_normal)
+                4 -> menuImg.setImageResource(R.drawable.menu_5_normal)
+            }
+            menuTxt.setTextColor(Color.parseColor("#404040"))
+        }
+        val curMenuImg : ImageView = (footerMenu.getChildAt(curFragmentPageIndex) as ViewGroup).getChildAt(0) as ImageView
+        val curMenuTxt : TextView = (footerMenu.getChildAt(curFragmentPageIndex) as ViewGroup).getChildAt(1) as TextView
+        when (curFragmentPageIndex) {
+            0 -> curMenuImg.setImageResource(R.drawable.menu_1)
+            1 -> curMenuImg.setImageResource(R.drawable.menu_2)
+            2 -> curMenuImg.setImageResource(R.drawable.menu_3)
+            3 -> curMenuImg.setImageResource(R.drawable.menu_4)
+            4 -> curMenuImg.setImageResource(R.drawable.menu_5)
+        }
+        curMenuTxt.setTextColor(Color.parseColor("#0f86d6"))
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
